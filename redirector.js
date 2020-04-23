564c2a2714ee1ca7828018e0dcacbb724aa5f4c0
maycowmoura <maycowmoura@gmail.com>
2020-04-23 20:32:15 -0300

    Update redirector.js

--- redirector.js
+++ redirector.js
@@ -1,6 +1,7 @@
 if (window.sessionStorage.path) {
   let path = window.sessionStorage.path;
   window.history.pushState(null, null, path);
+  window.sessionStorage.removeItem('path');
 } else {
   let path = window.location.pathname;
   window.sessionStorage.path = path;
