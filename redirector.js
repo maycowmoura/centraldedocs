71519679ca85234ec20a0e7fdf5b2932d6afb1c4
maycowmoura <maycowmoura@gmail.com>
2020-04-23 20:28:16 -0300

    Create redirector.js

---
+++ redirector.js
@@ -0,0 +1,8 @@
+if (window.sessionStorage.path) {
+  let path = window.sessionStorage.path;
+  window.history.pushState(null, null, path);
+} else {
+  let path = window.location.pathname;
+  window.sessionStorage.path = path;
+  window.location.href = '/';
+}
